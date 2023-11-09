6e1f449ca3b962555a6e22a0fe431d48c63071eb
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-09 19:41:57 +0600

    Initial Commit

---
+++ Tut3_SwiftUIState/Tut3_SwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tut3_SwiftUIStateApp.swift
+//  Tut3_SwiftUIState
+//
+//  Created by ZEUS on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Tut3_SwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
