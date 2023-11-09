1b9d5d7839e2a432f11beda31d1ba20772baac84
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-09 20:16:45 +0600

    State and Bindings

--- Tut3_SwiftUIState/ContentView.swift
+++ Tut3_SwiftUIState/ContentView.swift
@@ -8,19 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPlaying: Bool = true
+    @State private var tapCountRed: Int = 0
+    @State private var tapCountGreen: Int = 0
+    @State private var tapCountBlue: Int = 0
+
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                isPlaying.toggle()
+            } label: {
+                Image(systemName: isPlaying ?"stop.circle.fill" : "play.circle.fill")
+                    .font(.system(size: 100))
+                    .foregroundColor( isPlaying ? .red : .green)
         }
-        .padding()
+            VStack {
+                Text("\(tapCountRed + tapCountGreen + tapCountBlue)")
+                    .font(.system(size: 80, weight: .black, design: .rounded))
+                HStack {
+                    ExtractedView(counter: $tapCountRed, color: .red)
+                    ExtractedView(counter: $tapCountGreen, color: .green)
+                    ExtractedView(counter: $tapCountBlue, color: .accentColor)
+                }
+            }
+            Spacer()
+        }
+
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExtractedView: View {
+    @Binding var counter: Int
+    var color: Color
+    var body: some View {
+        Button {
+            counter += 1
+        } label: {
+            Circle()
+                .frame(width: 100, height: 100)
+                .foregroundColor(color)
+                .overlay {
+                    Text("\(counter)")
+                        .font(.system(size: 40, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                }
+        }
     }
 }
